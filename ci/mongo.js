c74cf849b65b87a1f1207a0e36bd9baa18bd8671
John McMahon <jmcmahon@radiantblue.com>
2017-05-31 10:50:22 -0400

    Update db script

--- ci/mongo.js
+++ ci/mongo.js
@@ -1,3 +1,3 @@
 conn = new Mongo();
 db = conn.getDB("Piazza")
-db.UserProfiles.insertOne({ "username" : "admin", "credential" : "$2a$12$YYOG/JBAY7keBYzk6tmUq.t1SkBgMaZ1I3Hr473l862cXuIm4DHz2", "distinguishedName" : "primary.admin", "createdBy" : "system", "adminCode" : "", "dutyCode" : "", "country" : "us", "npe" : false, "createdOn" : "2017-05-26T11:22:23.050-04:00", "lastUpdatedOn" : "2017-05-26T11:22:23.050-04:00" })
+db.UserProfiles.insertOne({"username" : "coastline", "credential" : "$2a$12$7EK1s7OevtebPqPSuCgls.Za6BWzUxYDS..HZBAcuHP74/2SDv5.i", "distinguishedName" : "coastline.admin", "createdBy" : "system", "adminCode" : "", "dutyCode" : "", "country" : "us", "npe" : false, "lastUpdatedOn" : "2017-05-31T10:35:50.306-04:00", "createdOn" : "2017-05-31T10:35:50.306-04:00" })
